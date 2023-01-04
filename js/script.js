d192b70ae866653725421de04943f264910fed27
Urszula Malinowska <ula.malin35@gmail.com>
2023-01-04 16:01:19 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,8 +10,19 @@
         },
     ];
 
-
-
+     
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+ 
+    render();
+    };
+     
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                    render();
+    }
     const render = () => {
         let htmlString = "";
     
@@ -20,22 +31,26 @@
             <li 
             ${task.done ? "style=\"text-decoration: line-through\"" : ""}
             >
+            <button class="js-remove">Usuń</button>
             ${task.content}
+            <button>zrobione</button>
             </li>
             `;
         }
     
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+                removeButton.addEventListener("click", () => {
+                    removeTask(index);
+                });
+        });
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
- 
-    render();
-    }
     
+
     const onFormSubmit = (event) => {
         event.preventDefault();
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
@@ -44,8 +59,11 @@
         } 
             addNewTask(newTaskContent);
         
-    }
+    };
 
+   
+
+   
     const init =  () => {
         render();
 
